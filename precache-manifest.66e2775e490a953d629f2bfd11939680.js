6635e361f01b3105aca455bc113c2e827fea8763
Jmay <justin.sean.may@gmail.com>
2020-02-26 10:23:47 -0500

    adding static files

---
+++ precache-manifest.66e2775e490a953d629f2bfd11939680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d7cd1b80b362f560369483612cc9505",
+    "url": "/SIGINT-crisis/index.html"
+  },
+  {
+    "revision": "e4820efbf509c3f886b9",
+    "url": "/SIGINT-crisis/static/css/main.3b449b19.chunk.css"
+  },
+  {
+    "revision": "1dec982b206f3469f8e8",
+    "url": "/SIGINT-crisis/static/js/2.17c90ce6.chunk.js"
+  },
+  {
+    "revision": "f32f3f2739f945f6e8e72f406ee3a70f",
+    "url": "/SIGINT-crisis/static/js/2.17c90ce6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4820efbf509c3f886b9",
+    "url": "/SIGINT-crisis/static/js/main.2c5e62e3.chunk.js"
+  },
+  {
+    "revision": "990052ff4d2de6b8dc0d",
+    "url": "/SIGINT-crisis/static/js/runtime-main.e2eae77e.js"
+  }
+]);
